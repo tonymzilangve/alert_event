859024ebb726ec5cbbdc4531c936b9ba22683c9b
Entoni Mzilangve <entoni.mzilangve@dualbootpartners.com>
2025-01-16 14:45:45 +0300

    Added fastapi filter

--- app/api/endpoints/alert_message.py
+++ app/api/endpoints/alert_message.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends
+from fastapi_filter import FilterDepends
+from fastapi_filter.base.filter import BaseFilterModel
 from fastapi_pagination import add_pagination, paginate, Page, Params
 from clickhouse_connect.driver.asyncclient import AsyncClient
 
@@ -19,18 +21,46 @@ async def convert_query_data(data: list[tuple]) -> list[dict]:
     return converted_data
 
 
+class AlertMessageFilter(BaseFilterModel):
+    type: AlertMessage.Types | None = None
+    severity: AlertMessage.Severities | None = None
+    acknowledged: bool | None = None
+    
+    def filter(self, query: str) -> str:
+        filters = []
+        
+        if self.type:
+            filters.append(f"(type = '{self.type}')")
+        if self.severity:
+            filters.append(f"(severity = '{self.severity}')")
+        if self.acknowledged:
+            filters.append(f"(acknowledged ='{int(self.acknowledged)}')")
+
+        if filters:
+            sep = ' AND '
+            filter_query = " WHERE ".join((query, sep.join(filters)))
+            return filter_query
+        else:
+            return query
+
+    class Constants(BaseFilterModel.Constants):
+        model = AlertMessage
+
+
 @router.get("/", response_model=Page[AlertMessage])
 async def fetch_alert_messages(
     size: int = 5, 
     page: int = 1,
-    db: AsyncClient = Depends(get_db)
+    db: AsyncClient = Depends(get_db),
+    msg_filter: AlertMessageFilter = FilterDepends(AlertMessageFilter)
 ) -> Page[AlertMessage]:
     query = "SELECT * FROM alert_messages"
-    params = Params(size=size, page=page)
-    
-    db_data = await db.query(query=query)
+    filtered_query = msg_filter.filter(query)
+
+    db_data = await db.query(query=filtered_query)
     messages = await convert_query_data(db_data)
     
+    params = Params(size=size, page=page)
     return paginate(messages, params)
 
 
--- app/api/schemas/alert_message.py
+++ app/api/schemas/alert_message.py
@@ -1,14 +1,24 @@
 from datetime import datetime
+from enum import Enum
 from pydantic import BaseModel
 from uuid import UUID
-from typing import Literal
 
 
 class AlertMessage(BaseModel):
+    class Types(str, Enum):
+        USER = "user"
+        DEVICE = "device"
+        SYSTEM = "system"
+
+    class Severities(str, Enum):
+        CRITICAL = "Critical"
+        WARNING = "Warning"
+        INFO = "Info"
+    
     uuid: UUID
     ts: datetime
-    type: Literal["user", "device", "system"]
-    severity: Literal["Critical", "Warning", "Info"]
+    type: Types
+    severity: Severities
     message: str
     source: str
     payload: str
